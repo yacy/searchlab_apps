2feef6dbdeda47ac078892f2c0a73876a488384b
Michael Peter Christen <mc@yacy.net>
2022-02-24 01:07:19 +0100

    moved websearch_lit into another subfolder 'app'

--- htdocs/_/js/conf.js
+++
@@ -1,2 +0,0 @@
-// modify this according to the location of your elasticsearch instance
-var search_api = "https://searchlab.eu/en/api/yacysearch.json";
---
+++ htdocs/app/_/js/conf.js
@@ -0,0 +1,14 @@
+// Searchlab Apps Settings
+//
+// Modify these settings according to the location of your Searchlab or Yacy instance.
+// Because Searchlab and YaCy ("YaCy P2p", "Legacy YaCy") have the same API endpoints (just at different paths)
+// it is possible to run the Searchlab Apps also against a YaCy P2P instance.
+// This applies also for YaCy Grid.
+
+var search_api  = "https://searchlab.eu/en/api/yacysearch.json";
+//var search_api = "http://searchlab.eu/yacy/grid/mcp/index/yacysearch.json';
+//var search_api = "http://localhost:8090/yacysearch.json";
+
+var suggest_api = "https://searchlab.eu/en/api/suggest.json";
+//var suggest_api = "http://searchlab.eu/yacy/grid/mcp/index/suggest.json";
+//var suggest_api = "http://localhost:8090/suggest.json";
