3bd6c8ea6a655f9652831cae1aa09377f32ca115
Michael Peter Christen <mc@yacy.net>
2022-02-24 01:21:27 +0100

    added websearch_bootstrap app (stub)

---
+++ htdocs/app/websearch_bootstrap/js/setup.js
@@ -0,0 +1,11 @@
+/*
+ * setup.js
+ * customization values for the yacy_webclient_bootstrap templates
+ * (C) 2014, 2015 by Michael Peter Christen, licensed by CC0
+ */
+
+var homepage="http://yacy.net";
+var logo="../images/YaCyLogo2011_60.png";
+var headline="Search Portal";
+var greeting="Document Search";
+var queryplaceholder="Document Retrieval";
