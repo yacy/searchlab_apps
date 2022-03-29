6b6fa4e9fb2a1a55d9c02fb4ffe31ec770e7d6ba
Michael Peter Christen <mc@yacy.net>
2022-03-29 10:56:32 +0200

    added more paths as demo path for the configuration

--- htdocs/app/_/js/conf.js
+++ htdocs/app/_/js/conf.js
@@ -5,10 +5,25 @@
 // it is possible to run the Searchlab Apps also against a YaCy P2P instance.
 // This applies also for YaCy Grid.
 
-var search_api  = "https://searchlab.eu/en/api/yacysearch.json";
-//var search_api = "http://searchlab.eu/yacy/grid/mcp/index/yacysearch.json';
+// ### Search API
+// ## Searchlab Paths
+var search_api = "https://searchlab.eu/en/api/yacysearch.json";
+//var search_api = "http://localhost:8400/en/api/yacysearch.json";
+
+// ## YaCy Grid Paths
+//var search_api = "http://searchlab.eu/yacy/grid/mcp/index/yacysearch.json";
+//vae search_api = "http://127.0.0.1:8100/yacy/grid/mcp/index/yacysearch.json";
+
+// ## YaCy P2P Paths
 //var search_api = "http://localhost:8090/yacysearch.json";
 
+// Suggest API
+// ## Searchlab Paths
 var suggest_api = "https://searchlab.eu/en/api/suggest.json";
+//var suggest_api = "http://localhost:8400/en/api/suggest.json";
+
+// ## YaCy Grid Paths
 //var suggest_api = "http://searchlab.eu/yacy/grid/mcp/index/suggest.json";
+
+// ## YaCy P2P Paths
 //var suggest_api = "http://localhost:8090/suggest.json";
